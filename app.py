aeaebed7ef91c981dad742a64d793387590b3d52
Saam Wesley <65556254+Saam09@users.noreply.github.com>
2023-09-19 22:41:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import mysql.connector
+import datetime
 
 app = Flask(__name__)
 
@@ -12,29 +13,36 @@ def hello():
 @app.route("/process_data", methods=["POST"])
 def process_data():
     s_no = request.form.get("student_roll_no")
-
-    # Connect to your SQLite database (change the database path accordingly)
+    time_slot = request.form.get("time_slot")
     conn = mysql.connector.connect(
         host="localhost", user="root", password="root", database="students"
     )
 
     cursor = conn.cursor()
 
-    # Execute a SQL query to retrieve the sname based on the student_roll_no
     cursor.execute("SELECT sname FROM student WHERE srno = %s", (s_no,))
     result = cursor.fetchone()
 
-    # Close the database connection
-    conn.close()
-
-    # Check if a result was found
     if result:
-        student_name = result[0]  # Extract the student name from the result
+        student_name = result[0]
+        attendance_status = "Present"
+        current_date = datetime.date.today()
+
+        cursor.execute(
+            "INSERT INTO attendance (srno, timeslot, attendance_status, attendance_date) VALUES (%s, %s, %s, %s)",
+            (s_no, time_slot, attendance_status, current_date),
+        )
+        conn.commit()
     else:
         student_name = "Student not found"
 
+    conn.close()
+
     return render_template(
-        "index.html", student_roll_no=s_no, student_name=student_name
+        "index.html",
+        student_roll_no=s_no,
+        student_name=student_name,
+        time_slot=time_slot,
     )
 
 
